d267416c30996f9365c6fed466f664cbb490113a
jwiz <jordanheath945@gmail.com>
2022-10-24 20:26:32 -0400

    developed api routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,81 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: Product
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: Product
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(tagData => res.json({"Success": "Tag Created!", "Tag": tagData}))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(tagData => {!tagData ? 
+    res.status(404).json({"message": "No Tag found with this id"}) :
+    res.json({"Success":"Tag has been updated", "Tag": tagData})
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {!tagData ? 
+    res.status(404).json({"message": "No Tag found with this id"}) :
+    res.json({"Success":"Tag has been deleted", "Tag": tagData})
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
